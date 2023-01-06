88824ee4d558090879df0add353d5344d74f1d49
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-06 16:42:47 +0100

    reset js

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-/* eslint-disable no-restricted-syntax */
-function comparePasswords() {
-  // returns true if passwords are the same
-  return passOne.value === passTwo.value;
-}
+const button = document.querySelector('#button')
 
-function checkValid() {
-  // if inputs are valid => return true else return false
-  const inputs = document.querySelectorAll('.input input');
 
-  for (const input of inputs) {
-    if (input.checkValidity() !== true) { return false; }
-  }
-  return true;
-}
-
-const button = document.querySelector('#button');
-const passOne = document.querySelector('#password-one');
-const passTwo = document.querySelector('#password-two');
-const form = document.querySelector('#theForm');
 
 button.addEventListener('click', () => {
-  if (comparePasswords()) { console.log('good passwords'); } else { console.log('wrong poasswords'); return; }
-  if (checkValid() !== true) {
-    console.log('invalid inputs'); return;
-  }console.log('valid input');
-  console.log('submit');
-  // form.submit();
-});
+    //check validity
+    console.log("check validity")
+    //submit
+    console.log("submit")
+})
+
+

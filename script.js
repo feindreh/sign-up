ade34c19ff1819773abf1f70ba71745d6a74f642
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-10 16:43:27 +0100

    simple js validation

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-const button = document.querySelector('#button')
+/* eslint-disable no-restricted-syntax */
 
-
+const button = document.querySelector('#button');
+const inputs = document.querySelectorAll('.input input');
+const pOne = document.querySelector('#password-one');
+const pTwo = document.querySelector('#password-two');
 
 button.addEventListener('click', () => {
-    //check validity
-    console.log("check validity")
-    //submit
-    console.log("submit")
-})
+  // check validity
+  if (checkPasswords() === false) { return; }
+  // submit
+  console.log('submit');
+});
 
+for (const input of inputs) {
+  input.addEventListener('input', () => {
+    if (input.checkValidity() === false) { showError(input); } else { hideError(input); }
+  });
+}
 
+function showError(input) {
+  const span = document.querySelector(`#${input.id}+span`);
+  span.innerHTML = 'invalid input';
+}
+
+function hideError(input) {
+  const span = document.querySelector(`#${input.id}+span`);
+  span.innerHTML = '';
+}
+
+function checkPasswords() {
+  if (pOne.value === pTwo.value) {
+    return true;
+  }
+  togglePasswords();
+}
+
+function togglePasswords() {
+  showError(pOne);
+  showError(pTwo);
+}
